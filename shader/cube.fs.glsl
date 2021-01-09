f87d633a2c592dfa7003ba2fad13a3c7d046489c
qcoumes <coumes.quentin@gmail.com>
2021-01-09 23:07:47 +0100

    Implementation of Cube shader

--- shader/cube.fs.glsl
+++ shader/cube.fs.glsl
@@ -3,15 +3,37 @@
 in vec3 vPosition;
 in vec3 vNormal;
 in vec2 vTexture;
+in vec2 vTextureOffset;
+flat in int vFace;
+flat in int vAlpha;
+flat in int vAnimated;
 
 uniform sampler2D uTexture;
 uniform vec3 uLightPosition;
 uniform vec3 uLightColor;
 uniform float uLightDirIntensity;
 uniform float uLightAmbIntensity;
+uniform int uVerticalOffset;
 
 out vec4 fFragColor;
 
+
+/**
+ * Compute the color of the fragment according to uTexture.
+ *
+ * @return The computed color.
+ */
+vec4 computeTextureColor() {
+    vec2 textureCoordinates = vec2((vTexture.x + vTextureOffset.x) / 8.f, 0);
+
+    if (vAnimated != 0) {
+        textureCoordinates.y = (vTexture.y + float(uVerticalOffset)) / 64.f;
+    } else {
+        textureCoordinates.y = (vTexture.y + (vTextureOffset.y * 6.f) + float(vFace)) / 32.f;
+    }
+
+    return texture(uTexture, textureCoordinates);
+}
 
 /**
  * Compute the diffuse lighting of the fragment.
@@ -20,14 +42,13 @@ out vec4 fFragColor;
  */
 vec3 computeDiffuseLighting() {
     vec3 lightDirection = normalize(uLightPosition - vPosition);
-    vec3 diffuse = vec3(max(dot(vNormal, lightDirection), 0.f));
+    vec3 diffuse = vec3(max(dot(vNormal, lightDirection), 0.0f));
     return diffuse * uLightDirIntensity;
 }
 
 
 void main() {
-    vec4 textureColor = texture(uTexture, vTexture);
-//    vec4 textureColor = vec4(1, 0, 0, 1);
+    vec4 textureColor = computeTextureColor();
     if (textureColor.w == 0.f) {
         discard;
     }
--- shader/cube.vs.glsl
+++ shader/cube.vs.glsl
@@ -3,23 +3,51 @@
 layout (location = 0) in vec3 aPosition;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexture;
+layout (location = 3) in int aData;
 
 uniform mat4 uMV;
 uniform mat4 uMVP;
 uniform mat4 uNormal;
+uniform vec3 uChunkPosition;
 
 out vec3 vPosition;
 out vec3 vNormal;
 out vec2 vTexture;
+out vec2 vTextureOffset;
+flat out int vFace;
+flat out int vAlpha;
+flat out int vAnimated;
 
+
+// Use to extract the bits 0b00000000.0000xxxx of aData, representing the X offset of the texture of the cube (&).
+const int TEXTURE_X = 15;
+
+// Use to extract the bits 0b00000000.xxxx0000 of aData, representing the Y offset of the texture of the cube (& >> 4).
+const int TEXTURE_Y = 240;
+
+// Use to extract the bits 0b00000xxx.00000000 of aData, representing the face of the cube (& >> 8).
+const int FACE = 1792;
+
+// Use to extract the bits 0b0000x000.00000000 of aData, telling if the cube has an alpha channel (&).
+const int ALPHA = 1 << 11;
+
+// Use to extract the bits 0b000x0000.00000000 of aData, telling if the cube has an animated texture (&).
+const int ANIMATED = 1 << 12;
+
+// Use to extract the bits 0b00Xx0000.00000000 of aData, telling the texture use an overlay according to the top of the cube (&).
+const int TOP_OVERLAY = 1 << 13;
 
 
 void main(){
-    vec4 vertexPosition = vec4(aPosition, 1.f);
+    vec4 vertexPosition = vec4(aPosition + uChunkPosition, 1);
 
     vPosition = vec3(uMV * vertexPosition);
-    vNormal = vec3(uNormal * vec4(aNormal, 0.f));
+    vNormal = vec3(uNormal * vec4(aNormal, 0));
     vTexture = aTexture;
+    vTextureOffset = vec2(aData & TEXTURE_X, (aData & TEXTURE_Y) >> 4 );
+    vFace = (aData & FACE) >> 8;
+    vAlpha = aData & ALPHA;
+    vAnimated = aData & ANIMATED;
 
     gl_Position = uMVP * vertexPosition;
 }
